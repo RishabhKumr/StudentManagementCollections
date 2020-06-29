1ffb79c1b55feb34258af5ffc94d91d3e05cc665
RishabhKumr <500054802@stu.upes.ac.in>
2020-06-29 23:07:49 +0530

    view added

---
+++ com/studentmanagement/view/StudentMobileApp.java
@@ -0,0 +1,61 @@
+package com.studentmanagement.view;
+
+import com.studentmanagement.model.UserInterface;
+
+public class StudentMobileApp implements UserInterface {
+
+	@Override
+	public void showFirstScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showStudentScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showAdminScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showAllStudentsScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showStudentRegistrationScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void introduceNewCoursesScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showAllCoursesScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enrollForCourse() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showAllEnrollments() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
